90f06e21d3e90a9f81f430d6cd0a068e6166d90c
Garret Gallant <gallanttechnological@gmail.com>
2022-07-01 11:32:20 -0600

    commit tree d9a24edcddfbb61434a6fbadf447d91d74b3dc6a

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// code your solution here
+// // code your solution here
+// // const findAWin = (element) => {
+
+// //     if (element !== undefined){
+// //         return element.year;
+// //         }
+// //         return element;
+//     // else if (element.result !== "W"){
+//     //     return undefined;
+//     // }
+// }
+
+// const superbowlWin = (array) => {
+//     return array.result === `W`;
+// }
+
+// return(superbowlWin(record));
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+]
+
+// step 1, check for a true/false value, verify conditions match
+// step 2, if conditions match, return the object year.
+
+function superbowlWin(record) {
+    const W = record.find(elem => elem.result === "W");
+    return W ? W.year : undefined;
+}
